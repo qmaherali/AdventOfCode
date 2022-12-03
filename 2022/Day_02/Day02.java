051a01e975cee347df69870204307feeb0fd8faa
qmaherali <115840706+qmaherali@users.noreply.github.com>
2022-12-03 00:26:08 -0500

    Create Day02.java

---
+++ 2022/Day_02/Day02.java
@@ -0,0 +1,81 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day02 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		ArrayList<String> input = readFile("Dec2_2022.txt");
+		
+		//Part 1
+		char[] elfMove = new char[input.size()];
+		char[] personMove = new char[input.size()];
+		int rocks = 0; int papers = 0; int scissors = 0;
+		int wins = 0; int draws = 0;
+		
+		for (int i = 0; i < input.size(); i++) {
+			elfMove[i] = input.get(i).charAt(0);
+			personMove[i] = input.get(i).charAt(input.get(i).length()-1);
+		}
+		
+		for (int i = 0; i < input.size(); i++) {
+			if (personMove[i] == 'X')
+				rocks++;
+			else if (personMove[i] == 'Y')
+				papers++;
+			else if (personMove[i] == 'Z') 
+				scissors++;
+			if ((personMove[i] == 'X' && elfMove[i] == 'C') || (personMove[i] == 'Y' && elfMove[i] == 'A') || (personMove[i] == 'Z' && elfMove[i] == 'B'))
+				wins++;
+			else if ((personMove[i] == 'X' && elfMove[i] == 'A') || (personMove[i] == 'Y' && elfMove[i] == 'B') || (personMove[i] == 'Z' && elfMove[i] == 'C'))
+				draws++;
+		}
+		
+		int points = rocks + (papers*2) + (scissors*3) + (draws*3) + (wins*6);
+		System.out.println("Part 1: " + points);
+		
+		
+		//Part 2
+		rocks = 0;
+		papers = 0;
+		scissors = 0;
+		wins = 0;
+		draws = 0;
+		points = 0;
+		
+		for (int i = 0; i < input.size(); i++) {
+			if (personMove[i] == 'Y')
+				draws++;
+			else if (personMove[i] == 'Z') 
+				wins++;
+			if ((personMove[i] == 'X' && elfMove[i] == 'B') || (personMove[i] == 'Y' && elfMove[i] == 'A') ||  (personMove[i] == 'Z' && elfMove[i] == 'C'))
+				rocks++;
+			if ((personMove[i] == 'X' && elfMove[i] == 'C') || (personMove[i] == 'Y' && elfMove[i] == 'B') ||  (personMove[i] == 'Z' && elfMove[i] == 'A'))
+				papers++;
+			if ((personMove[i] == 'X' && elfMove[i] == 'A') || (personMove[i] == 'Y' && elfMove[i] == 'C') ||  (personMove[i] == 'Z' && elfMove[i] == 'B'))
+				scissors++;
+		}
+		
+		points = rocks + (papers*2) + (scissors*3) + (draws*3) + (wins*6);
+		System.out.println("Part 2: " + points);
+		
+	}
+	
+	public static ArrayList<String> readFile (String fileName) throws FileNotFoundException {
+		
+		ArrayList<String> input = new ArrayList<String>();
+		
+		File inFile = new File(fileName);
+		Scanner scan = new Scanner(inFile);
+		
+		while (scan.hasNext()) {
+			input.add(scan.nextLine());
+		}
+    
+		scan.close();
+		return input;
+    
+	}
+}
